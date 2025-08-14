c8690eeebbb534744e2c4e8a355760b4724e406f
Lara Ismael <lara8ismael@gmail.com>
2025-08-14 12:29:30 -0700

    update style with tailwind

--- src/QrCode.tsx
+++ src/QrCode.tsx
@@ -3,21 +3,19 @@ import "tailwindcss";
 
 const QrCode: React.FC = () => {
   return (
-    <div className="App">
-      <main className="card p-5 rounded-lg">
-        <img
-          src="./assets/image-qr-code.png"
-          alt="QR code"
-          className="image w-80 rounded-lg"
-        />
-        <h1 className="text-2xl mt-5 header mb-5 text-center w-80">
-          Improve your front-end skills by building projects
-        </h1>
-        <p className="about pb-7 text-sm text-center w-80">
-          Scan the QR code to visit Frontend Mentor and take your coding skills
-          to the next level
-        </p>
-      </main>
+    <div className="bg-white rounded-2xl shadow-lg p-4 max-w-xs text-center">
+      <img
+        src="./assets/image-qr-code.png"
+        alt="QR code"
+        className="rounded-lg mb-4"
+      />
+      <h1 className="text-xl font-bold text-slate-800 mb-2">
+        Improve your front-end skills by building projects
+      </h1>
+      <p className="text-slate-500 text-sm mb-4">
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
     </div>
   );
 };
